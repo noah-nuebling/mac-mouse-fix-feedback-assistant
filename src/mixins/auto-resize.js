28fd126b13fd34f890b29835cabaeb1baf185a5c
noah-gk <noah.n.public@gmail.com>
2020-10-09 15:50:41 +0200

    Removed console spam

--- src/mixins/auto-resize.js
+++ src/mixins/auto-resize.js
@@ -4,13 +4,13 @@ export default {
     methods: {
         mixin_autoResize_resize(target) {
 
-            console.log('RESISIZNG AUTOMATICALLY')
-
             target.style.height = "auto";
             let h = target.scrollHeight
             target.style.height = `${h}px`;
         }
     },
+
+    // v I copied this form somewhere on the internet, not sure what it's good for. I don't seem to need it
     // mounted() {
     //     this.$nextTick(() => {
     //         this.$el.setAttribute("style", "height",
